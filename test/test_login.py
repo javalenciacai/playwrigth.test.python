f97254d35fdfef46f6602615a75f5e64a560a1f8
James Alberto Valencia Caicedo <javalenciacai@gmail.com>
2022-11-08 07:48:28 -0500

    add print to test login front manager

--- test/test_login.py
+++ test/test_login.py
@@ -30,6 +30,8 @@ class TestLogin:
     @pytest.mark.parametrize("testdata", [(DataLogin.qa_mexico)])
     def test_login_front_manager(self, page: Page, testdata) -> None:
         i = 0
+        print(testdata[i]["url"])
+        print(testdata[i]["username"])
         GeneralSteps.start_navigate(page,testdata[i]["url"], testdata[i]["title"])
         LoginSteps.login_from_manager(page, testdata[i]["username"], "1111")
         LoginSteps.validate_company_on_update(page)        
